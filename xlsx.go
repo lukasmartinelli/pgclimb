69b68c533b213079f859954441f1eeb406b32f90
Lukas Martinelli <me@lukasmartinelli.ch>
2016-02-09 10:57:41 +0100

    Add XLSX support

---
+++ xlsx.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/tealeg/xlsx"
+
+	"fmt"
+)
+
+type XlsxEncoder struct {
+	file  *xlsx.File
+	sheet *xlsx.Sheet
+}
+
+func NewXlsxEncoder() *XlsxEncoder {
+
+	file := xlsx.NewFile()
+	sheet, _ := file.AddSheet("data")
+
+	return &XlsxEncoder{file, sheet}
+}
+
+func (e *XlsxEncoder) Encode(values map[string]interface{}) error {
+	row := e.sheet.AddRow()
+
+	for _, value := range values {
+		cell := row.AddCell()
+		switch value := (value).(type) {
+		case []byte:
+			cell.Value = string(value)
+		case int64:
+			cell.Value = fmt.Sprintf("%d", value)
+		}
+	}
+	return nil
+}
