54e7036812a0e0ed65f0ea8223a037a43c6183d2
Lukas Martinelli <me@lukasmartinelli.ch>
2016-02-16 19:07:38 +0100

    Deal with nested JSON objects

--- formats/json.go
+++ formats/json.go
@@ -19,8 +19,33 @@ func (e *JSONFormat) WriteHeader(columns []string) error {
 }
 
 func (e *JSONFormat) WriteRow(rows map[string]interface{}) error {
-	rows = convertBytesToString(rows)
+	rows = convertToJSON(rows)
 	err := e.encoder.Encode(rows)
+	return err
+}
+
+func convertToJSON(rows map[string]interface{}) map[string]interface{} {
+	for k, v := range rows {
+		switch v := (v).(type) {
+		case []byte:
+			var jsonVal interface{}
+			err := json.Unmarshal(v, &jsonVal)
+			if err == nil {
+				rows[k] = jsonVal
+			} else {
+				rows[k] = string(v)
+			}
+		default:
+			rows[k] = v
+		}
+	}
+	return rows
+}
+
+// Try to JSON decode the bytes
+func tryUnmarshal(b []byte) error {
+	var v interface{}
+	err := json.Unmarshal(b, &v)
 	return err
 }
 
